492bf354996916ff637a6143fc1d84419cdc068c
Abdul Hameed <32898608+abduham@users.noreply.github.com>
2020-10-05 19:34:47 +0530

    Add files via upload

---
+++ ExtensionMethod.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EMR.PSS.SOFT.CRM.Utility
+{
+    public static class ExtensionMethod
+    {
+        /// <summary>
+        /// Check if String is Null or Empty
+        /// </summary>
+        /// <param name="inputValue"></param>
+        /// <returns></returns>
+        public static bool IsNullOrEmptyOrWhiteSpace(this string inputValue)
+        {
+            if (string.IsNullOrEmpty(inputValue))
+                return true;
+            return string.IsNullOrWhiteSpace(inputValue.Trim());
+        }
+
+
+        public static bool HasProperty(this object valueToCheck, string propertyName)
+        {
+            var type = valueToCheck.GetType();
+            return type.GetProperty(propertyName) != null;
+        }
+
+        /// <summary>
+        /// Sets the property value.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="TAverage">The type of the average.</typeparam>
+        /// <param name="sourceObject">The source object.</param>
+        /// <param name="propertyName">Name of the property.</param>
+        /// <param name="propertyValue">The property value.</param>
+        /// <exception cref="System.ArgumentNullException">
+        /// sourceObject
+        /// or
+        /// propertyName
+        /// or
+        /// propertyValue
+        /// </exception>
+        /// <exception cref="System.Exception">Property Type supplied is not matching with Object, It should be of Type  + typeOfT.GetProperty(propertyName).Name + ( + typeOfT.GetProperty(propertyName) + )</exception>
+        public static void SetPropertyValue<T, TAverage>(this T sourceObject, string propertyName, TAverage propertyValue) where T : new()
+        {
+            if (null == sourceObject) throw new ArgumentNullException("sourceObject");
+            if (string.IsNullOrWhiteSpace(propertyName)) throw new ArgumentNullException("propertyName");
+            if (null == propertyValue) throw new ArgumentNullException("propertyValue");
+            var typeOfT = DictionaryType.GetObjectType<T>(typeof(T).Name, string.Empty, string.Empty, string.Empty, "neutral");
+            string fullName = propertyValue.GetType().FullName;
+            if (fullName != null && !fullName.Equals("System.DBNull", StringComparison.Ordinal))
+            {
+                typeOfT.GetProperty(propertyName).SetValue(sourceObject, propertyValue, null);
+            }
+        }
+
+        public static DateTime? GetDateFormat(this string text)
+        {
+            return text.Length == 0 ? (DateTime?)null : Convert.ToDateTime(text, CultureInfo.InvariantCulture);
+        }
+
+        public static string GetFileExtension(this string fileName)
+        {
+            if (fileName.IndexOf('.') > 0)
+            {
+                string[] _FileCompenent = fileName.Split('.');
+                return _FileCompenent[_FileCompenent.Length - 1];
+            }
+            else
+                return string.Empty;
+        }
+    }
+
+    public class PropertyComparer<T> : IEqualityComparer<T>
+    {
+        private System.Reflection.PropertyInfo propertyInfo;
+        /// <summary>
+        /// Creates a new instance of PropertyComparer.
+        /// </summary>
+        /// <param name="propertyName">The name of the property on type T 
+        /// to perform the comparison on.</param>
+        public PropertyComparer(string propertyName)
+        {
+            //store a reference to the property info object for use during the comparison
+            propertyInfo = typeof(T).GetProperty(propertyName, BindingFlags.GetProperty | BindingFlags.Instance | BindingFlags.Public);
+            if (propertyInfo == null)
+            {
+                throw new ArgumentException(string.Format(CultureInfo.InvariantCulture, "{0} is not a property of type {1}.", propertyName, typeof(T)));
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        public bool Equals(T x, T y)
+        {
+            //get the current value of the comparison property of x and of y
+            object xValue = propertyInfo.GetValue(x, null);
+            object yValue = propertyInfo.GetValue(y, null);
+            //if the xValue is null then we consider them equal if and only if yValue is null
+            if (xValue == null)
+                return yValue == null;
+            //use the default comparer for whatever type the comparison property is.
+            return xValue.Equals(yValue);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public int GetHashCode(T obj)
+        {
+            //get the value of the comparison property out of obj
+            object propertyValue = propertyInfo.GetValue(obj, null);
+            return (propertyValue == null) ? 0 : propertyValue.GetHashCode();
+        }
+
+    }
+
+    public static class DictionaryType
+    {
+        private static Dictionary<string, Type> dictionaryMappings = new Dictionary<string, Type>();
+
+        public static Type GetObjectType<T>(string typeName, string assemblyFilePath, string version, string publicKeyToken, string culture) where T : new()
+        {
+            //TODO: May be Get This value From Cache
+            if (null == dictionaryMappings)
+            {
+                dictionaryMappings = new Dictionary<string, Type>();
+            }
+            if (!dictionaryMappings.Keys.Contains(typeName))
+            {
+                if (string.IsNullOrWhiteSpace(assemblyFilePath))
+                {
+                    dictionaryMappings.Add(typeName, typeof(T));
+                }
+                else if (version.IsNullOrEmptyOrWhiteSpace() && publicKeyToken.IsNullOrEmptyOrWhiteSpace() && culture.IsNullOrEmptyOrWhiteSpace())
+                {
+                    dictionaryMappings.Add(typeName, ((T)Activator.CreateInstance(Assembly.LoadFrom(assemblyFilePath).GetType(typeof(T).Name))).GetType());
+                }
+                else
+                {
+                    dictionaryMappings.Add(typeName, ((T)Activator.CreateInstance(Assembly.Load(string.Format(CultureInfo.InvariantCulture, "{0}, Version={1}, Culture={2}, PublicKeyToken={3}", assemblyFilePath, version, culture, publicKeyToken)).GetType(typeof(T).Name))).GetType());
+                }
+            }
+            return dictionaryMappings[typeName];
+        }
+    }
+
+   
+}
---
+++ XMLSerializer.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+using System.IO;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.Xml;
+
+namespace EMR.PSS.SOFT.CRM.Utility
+{
+    public static class XMLSerializer
+    {
+        /// <summary>
+        ///     Gets the XML from list.
+        /// </summary>
+        /// <param name="listObject">The list object.</param>
+        /// <returns></returns>
+        public static string GetXmlFromList(object value)
+        {
+            if (value != null)
+            {
+                var swListObject = new StringWriter(new StringBuilder(), CultureInfo.InvariantCulture);
+                (new XmlSerializer(value.GetType())).Serialize(swListObject, value);
+                return RemoveAllNamespaces(swListObject.ToString());
+            }
+            else { return null; }
+        }
+
+        public static string GetXmlRemoveEmptyElement(object value)
+        {
+            if (value != null)
+            {
+                var swListObject = new StringWriter(new StringBuilder(), CultureInfo.InvariantCulture);
+                (new XmlSerializer(value.GetType())).Serialize(swListObject, value);
+                return RemoveEmptyElementNamespaces(swListObject.ToString());
+            }
+            else { return null; }
+        }
+
+        public static string RemoveEmptyElements(string inputXmlValue)
+        {
+            if (string.IsNullOrEmpty(inputXmlValue)) return string.Empty;
+            var docXml = XElement.Parse(inputXmlValue);
+            docXml.Descendants().ToList().Where(d => d.Value.Length == 0).Remove();
+            return docXml.ToString();
+        }
+
+        /// <summary>
+        ///     Removes all namespaces.
+        /// </summary>
+        /// <param name="xmlDocument">The XML document.</param>
+        /// <returns></returns>
+        /// 
+
+        //Just commented-10/12/15
+
+        public static string RemoveEmptyElementNamespaces(string xmlDocument)
+        {
+            return RemoveAllNamespaces(XElement.Parse(RemoveEmptyElements(xmlDocument))).ToString();
+        }
+
+        public static string RemoveAllNamespaces(string xmlDocument)
+        {
+            return RemoveAllNamespaces(XElement.Parse(xmlDocument)).ToString();
+        }
+
+        public static XDocument LoadXml(string path)
+        {
+            XDocument document = XDocument.Load(path);
+
+            return document;
+        }
+
+
+        /// <summary>
+        ///     Removes all namespaces.
+        /// </summary>
+        /// <param name="xmlDocument">The XML document.</param>
+        /// <returns></returns>
+        private static XElement RemoveAllNamespaces(XElement xmlDocument)
+        {
+            if (xmlDocument != null)
+            {
+                if (!xmlDocument.HasElements)
+                {
+                    var xElement = new XElement(xmlDocument.Name.LocalName)
+                    {
+                        Value = xmlDocument.Value
+                    };
+                    return xElement;
+                }
+                return new XElement(xmlDocument.Name.LocalName, xmlDocument.Elements().Select(RemoveAllNamespaces));
+            }
+            else { return null; }
+        }
+
+        /// <summary>
+        ///     Deserializes the parameters.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="document">The document.</param>
+        /// <returns></returns>
+        public static Collection<T> DeserializeParams<T>(XNode document)
+        {
+            if (document == null) return null;
+            var serializer = new XmlSerializer(typeof(Collection<T>));
+            XmlReader reader = document.CreateReader();
+            var result = (Collection<T>)serializer.Deserialize(reader);
+            reader.Close();
+            return result;
+        }
+
+        /// <summary>
+        ///     Deserializes the specified XML.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="xml">The XML.</param>
+        /// <returns></returns>
+        public static T Deserialize<T>(string xml)
+        {
+            var xs = new XmlSerializer(typeof(T));
+            return (T)xs.Deserialize(new StringReader(xml));
+        }
+
+        /// <summary>
+        ///     Deserializes the specified input.
+        /// </summary>
+        /// <param name="input">The input.</param>
+        /// <param name="toType">To type.</param>
+        /// <returns></returns>
+        public static object Deserialize(string input, Type toType)
+        {
+            var ser = new XmlSerializer(toType);
+            using (var sr = new StringReader(input))
+            {
+                return ser.Deserialize(sr);
+            }
+        }
+
+        /// <summary>
+        ///     Deserializes from string.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="value">The value.</param>
+        /// <returns></returns>
+        public static T DeserializeFromString<T>(string value)
+        {
+            var deserializer = new XmlSerializer(typeof(T));
+            TextReader tr = new StringReader(value);
+            var outObject = (T)deserializer.Deserialize(tr);
+            return outObject;
+        }
+
+        /// <summary>
+        ///     Generics the de serialize.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="xml">The XML.</param>
+        /// <returns></returns>
+        public static Collection<T> GenericDeserialize<T>(string xml)
+        {
+            if (xml == null) return null;
+            var serializer = new XmlSerializer(typeof(Collection<T>)); //Creating XmlSerializer for the object
+            TextReader xmltextReader = new StringReader(xml); //Geting XMl from the variable. 
+            var deserialize = (Collection<T>)serializer.Deserialize(xmltextReader); //Deserialize back to object from XML
+            xmltextReader.Close();
+            return deserialize;
+        }
+
+        /// <summary>
+        /// Serialize an object to xml stirng
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="toSerialize"></param>
+        /// <returns></returns>
+        public static string SerializeObject<T>(this T toSerialize)
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(toSerialize.GetType());
+            using (StringWriter textWriter = new StringWriter(CultureInfo.InvariantCulture))
+            {
+                xmlSerializer.Serialize(textWriter, toSerialize);
+                return textWriter.ToString();
+            }
+        }
+    }
+}
